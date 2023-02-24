c9a717f06741c1b9de156ec1e396e66f6e38e870
pharaok <farouk.tamer03@gmail.com>
2023-02-24 06:49:41 +0200

    install prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className="text-red-600">
-      <p>hello world</p>
+    <main>
+      <p className="text-red-600">hello world</p>
     </main>
   );
 }
