72fd1dcfef32e2a13161233cfa711d63b5fc03c5
pharaok <farouk.tamer03@gmail.com>
2023-03-13 01:26:03 +0200

    create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import { Silkscreen } from "next/font/google";
+import Header from "components/header";
 import "./globals.css";
+
+const silkscreen = Silkscreen({
+  variable: "--font-silkscreen",
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: {
@@ -19,7 +27,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={silkscreen.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div
+      className="flex justify-between [&>*]:mx-8 mb-4 py-1 bg-gray-200 shadow-md shadow-black/25
+                 border-t-4 border-t-purple-600 border-b-2"
+    >
+      <h1 className="text-3xl font-display">Nonogram</h1>
+    </div>
+  );
+}
