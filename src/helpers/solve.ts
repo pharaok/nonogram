b733863de2de977327fd7cd8e3bf55f5c6063330
pharaok <farouk.tamer03@gmail.com>
2023-03-15 23:45:47 +0200

    refactor solveNonogram

--- src/helpers/solve.ts
+++ src/helpers/solve.ts
@@ -2,9 +2,16 @@ import { transpose } from "helpers";
 import produce from "immer";
 import { clueFromRow } from "./clue";
 
+// the minimum space required is the sum of the clue
+// + the gaps between lines
+//     1234 123
+// eg: #### ### = (4 + 3) + 2 - 1 = 8
+const minLength = (clue: number[]) =>
+  clue.reduce((r, c) => r + c, 0) + clue.length - 1;
+
 // O(2^(n*m))
 export const solveNonogram = (rowClues: number[][], colClues: number[][]) => {
-  const [width, height] = [colClues.length, rowClues.length];
+  const [height, width] = [rowClues.length, colClues.length];
   let solutions: number[][][] = [];
   const backtrack = (grid: number[][], i: number) => {
     if (i == width * height) {
@@ -12,38 +19,30 @@ export const solveNonogram = (rowClues: number[][], colClues: number[][]) => {
       return;
     }
     const [y, x] = [Math.floor(i / width), i % width];
+
     let [rc, cc] = [clueFromRow(grid[y]), clueFromRow(transpose(grid)[x])];
-    let [h, w] = [rc.length, cc.length];
     // we can leave the current cell empty if the last line's length is correct
     // and there is enough space left to finish the rest of the clue
-    // the minimum space required is the sum of the rest of the clue
-    // + the number of lines left - 1
-    //     1234 123
-    // eg: #### ### = (4 + 3) + 2 - 1 = 8
     if (
-      rc[h - 1] === rowClues[y][h - 1] &&
-      cc[w - 1] === colClues[x][w - 1] &&
-      rowClues[y].slice(h).reduce((r, c) => r + c, 0) +
-        Math.max(0, rowClues[y].length - h - 1) <=
-        width - x - 1 &&
-      colClues[x].slice(w).reduce((r, c) => r + c, 0) +
-        Math.max(0, colClues[x].length - w - 1) <=
-        height - y - 1
+      rc[rc.length - 1] === rowClues[y][rc.length - 1] &&
+      cc[cc.length - 1] === colClues[x][cc.length - 1] &&
+      minLength(rowClues[y].slice(rc.length)) <= width - x - 1 &&
+      minLength(colClues[x].slice(cc.length)) <= height - y - 1
     ) {
       backtrack(grid, i + 1);
     }
 
-    const ng = produce(grid, (draft) => {
+    grid = produce(grid, (draft) => {
       draft[y][x] = 1;
     });
-    let [nrc, ncc] = [clueFromRow(ng[y]), clueFromRow(transpose(ng)[x])];
+    [rc, cc] = [clueFromRow(grid[y]), clueFromRow(transpose(grid)[x])];
     // we can fill the cell if the current line length will be
     // less than or equal to that of the clue's
     if (
-      nrc[nrc.length - 1] <= rowClues[y][nrc.length - 1] &&
-      ncc[ncc.length - 1] <= colClues[x][ncc.length - 1]
+      rc[rc.length - 1] <= rowClues[y][rc.length - 1] &&
+      cc[cc.length - 1] <= colClues[x][cc.length - 1]
     ) {
-      backtrack(ng, i + 1);
+      backtrack(grid, i + 1);
     }
   };
   backtrack(
