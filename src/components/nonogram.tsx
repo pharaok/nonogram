269db2afe4954273c41f51e8b3f4990e212a3ab9
pharaok <farouk.tamer03@gmail.com>
2023-04-16 07:08:18 +0200

    draw grid lines

--- src/components/nonogram.tsx
+++ src/components/nonogram.tsx
@@ -1,18 +1,28 @@
 import Canvas2D from "helpers/canvas";
+import { totalmem } from "os";
 import { useEffect, useRef } from "react";
-import useNonogramStore from "store";
+import useNonogramStore, { selectClues } from "store";
 
 export default function Nonogram() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvas = useRef<Canvas2D | null>(null);
   const grid = useNonogramStore((state) => state.grid);
+  const clues = useNonogramStore(selectClues);
+  const [clueWidth, clueHeight] = [
+    Math.max(...clues[0].map((c) => c.length)),
+    Math.max(...clues[1].map((c) => c.length)),
+  ];
+  const [totalWidth, totalHeight] = [
+    grid[0].length + clueWidth,
+    grid.length + clueHeight,
+  ];
 
   useEffect(() => {
     canvas.current = new Canvas2D(canvasRef.current!, [
       0,
       0,
-      grid[0].length,
-      grid.length,
+      totalWidth,
+      totalHeight,
     ]);
     draw();
 
@@ -29,7 +39,27 @@ export default function Nonogram() {
   }, []);
 
   const draw = () => {
-    canvas.current!.drawRect(1, 1, 1, 1, "red");
+    for (let i = 0; i <= totalHeight; i++) {
+      canvas.current!.drawLine(
+        [
+          [0, i],
+          [totalWidth, i],
+        ],
+        1,
+        "black"
+      );
+    }
+    for (let i = 0; i <= totalWidth; i++) {
+      canvas.current!.drawLine(
+        [
+          [i, 0],
+          [i, totalHeight],
+        ],
+        1,
+        "black"
+      );
+    }
+    // canvas.current!.drawRect(1, 1, 1, 1, "red");
   };
   return <canvas ref={canvasRef} className="h-full w-full"></canvas>;
 }
--- src/helpers/canvas.ts
+++ src/helpers/canvas.ts
@@ -1,3 +1,7 @@
+type FillStyle = typeof CanvasRenderingContext2D.prototype.fillStyle;
+type StrokeStyle = typeof CanvasRenderingContext2D.prototype.strokeStyle;
+type Point = [number, number];
+
 export default class Canvas2D {
   readonly ctx: CanvasRenderingContext2D;
   viewBox: [number, number, number, number];
@@ -10,29 +14,42 @@ export default class Canvas2D {
     this.viewBox = viewBox ?? [0, 0, canvasEl.width, canvasEl.height];
   }
 
-  drawRect(
-    x: number,
-    y: number,
-    w: number,
-    h: number,
-    fill: typeof CanvasRenderingContext2D.prototype.fillStyle
-  ) {
-    const [ratioX, ratioY] = this.getViewBoxRatio();
-    this.ctx.fillStyle = fill;
-    this.ctx.fillRect(
-      (x - this.viewBox[0]) * ratioX,
-      (y - this.viewBox[1]) * ratioY,
-      w * ratioX,
-      h * ratioY
-    );
+  toPixel(x: number, y: number, c = true): [number, number] {
+    const [ratioX, ratioY] = this.getViewBoxRatio(c);
+    return [(x - this.viewBox[0]) * ratioX, (y - this.viewBox[1]) * ratioY];
   }
 
-  getViewBoxRatio() {
+  drawRect(x: number, y: number, w: number, h: number, fill: FillStyle) {
+    const [ratioX, ratioY] = this.getViewBoxRatio();
+    this.ctx.fillStyle = fill;
+    this.ctx.fillRect(...this.toPixel(x, y), w * ratioX, h * ratioY);
+  }
+
+  getViewBoxRatio(c = true): [number, number] {
     const [viewBoxWidth, viewBoxHeight] = [
       this.viewBox[2] - this.viewBox[0],
       this.viewBox[3] - this.viewBox[1],
     ];
     const { width, height } = this.ctx.canvas;
-    return [width / viewBoxWidth, height / viewBoxHeight];
+    return [(width - +c) / viewBoxWidth, (height - +c) / viewBoxHeight];
+  }
+
+  drawLine(points: Point[], lineWidth: number, stroke: StrokeStyle) {
+    if (!points.length) return;
+    this.ctx.lineWidth = lineWidth;
+    this.ctx.strokeStyle = stroke;
+
+    const getCenter = (p: Point) =>
+      this.toPixel(...p, lineWidth % 2 === 1).map(
+        (pp) => Math.round(pp) + (lineWidth % 2) / 2
+      ) as [number, number];
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(...getCenter(points[0]));
+    for (let i = 1; i < points.length; i++) {
+      this.ctx.lineTo(...getCenter(points[i]));
+    }
+    this.ctx.stroke();
+    this.ctx.closePath();
   }
 }
