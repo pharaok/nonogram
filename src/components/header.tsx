4d8acb2d880e4c99c626f4bff8d734679237d0c3
pharaok <farouk.tamer03@gmail.com>
2023-05-04 09:54:29 +0300

    make settings persist in local storage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header
-      className="mb-4 flex items-center  justify-between border-t-4 border-b-2 border-t-purple-600 bg-gray-200
+      className="mb-4 flex items-center  justify-between border-t-4 border-b-2 border-t-primary bg-gray-200
                  py-1 shadow-md shadow-black/25"
     >
       <nav className="flex items-center">
--- src/settings.ts
+++ src/settings.ts
@@ -1,35 +1,74 @@
 import produce from "immer";
 import { create } from "zustand";
-
-interface Keys {
-  cursorUp: string[];
-  cursorRight: string[];
-  cursorDown: string[];
-  cursorLeft: string[];
-  erase: string[];
-  brush1: string[];
-  brush2: string[];
-}
+import { persist } from "zustand/middleware";
 
 interface Settings {
-  keys: Keys;
-  setKeys: (action: keyof Keys, keys: string[]) => void;
+  colors: {
+    primary: string;
+    secondary: string;
+  };
+  keys: {
+    cursorUp: string[];
+    cursorRight: string[];
+    cursorDown: string[];
+    cursorLeft: string[];
+    erase: string[];
+    brush1: string[];
+    brush2: string[];
+  };
+  setColor: (color: keyof Settings["colors"], value: string) => void;
+  setKeys: (action: keyof Settings["keys"], keys: string[]) => void;
 }
 
-export const useSettings = create<Settings>((set) => ({
-  keys: {
-    cursorLeft: ["ArrowLeft", "h"],
-    cursorDown: ["ArrowDown", "j"],
-    cursorUp: ["ArrowUp", "k"],
-    cursorRight: ["ArrowRight", "l"],
-    erase: ["z", "Z", "Backspace"],
-    brush1: ["x", "X", " ", "Enter"],
-    brush2: ["c", "C"],
-  },
-  setKeys: (action, keys) =>
-    set((state) =>
-      produce(state, (draft) => {
-        draft.keys[action] = keys;
-      })
-    ),
-}));
+export const useSettings = create(
+  persist<Settings>(
+    (set, get) => ({
+      colors: {
+        primary: "147 51 234",
+        secondary: "37 99 235",
+      },
+      keys: {
+        cursorLeft: ["ArrowLeft", "h"],
+        cursorDown: ["ArrowDown", "j"],
+        cursorUp: ["ArrowUp", "k"],
+        cursorRight: ["ArrowRight", "l"],
+        erase: ["z", "Z", "Backspace"],
+        brush1: ["x", "X", " ", "Enter"],
+        brush2: ["c", "C"],
+      },
+      setKeys: (action, keys) =>
+        set(
+          produce(get(), (draft) => {
+            draft.keys[action] = keys;
+          })
+        ),
+      setColor: (color, value) => {
+        (document.querySelector(":root") as HTMLElement).style.setProperty(
+          `--color-${color}`,
+          value
+        );
+        return set(
+          produce(get(), (draft) => {
+            draft.colors[color] = value;
+          })
+        );
+      },
+    }),
+    {
+      name: "settings-storage",
+      onRehydrateStorage: () => {
+        return (state) => {
+          if (state === undefined) return;
+          const colors = state.colors;
+          let color: keyof typeof colors;
+          for (color in colors) {
+            (document.querySelector(":root") as HTMLElement).style.setProperty(
+              `--color-${color}`,
+              colors[color]
+            );
+          }
+        };
+      },
+    }
+  )
+);
