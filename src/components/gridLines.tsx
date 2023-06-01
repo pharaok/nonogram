acd79301082848833a4a4b874a1ba082466c019f
pharaok <farouk.tamer03@gmail.com>
2023-06-01 05:01:01 +0300

    fix gridLines

--- src/components/gridLines.tsx
+++ src/components/gridLines.tsx
@@ -1,4 +1,4 @@
-import Canvas2D, { drawGridLines } from "helpers/canvas";
+import Canvas2D, { Line, Rect } from "helpers/canvas";
 import { useParentDimensions } from "hooks";
 import { useCallback, useEffect, useRef } from "react";
 import useNonogramStore, { selectClues, selectDimensions } from "store";
@@ -17,78 +17,79 @@ export default function GridLines() {
     Math.max(...clues[1].map((c) => c.length)),
   ];
 
-  const [totalWidth, totalHeight] = [width + clueWidth, height + clueHeight];
   const canvasDim = useParentDimensions(canvasEl);
 
-  const draw = useCallback(() => {
-    canvas.current!.clear();
-
-    const getLineWidth = (a: number, i: number) => {
-      const j = i - [clueWidth, clueHeight][a];
-      if (j % 5 === 0) return 2;
-      if (j === 0 || j === [width, height][a]) return 2;
+  const getLineWidth = useCallback(
+    (a: number, i: number) => {
+      if (i % 5 === 0) return 2;
+      if (i === 0 || i === [width, height][a]) return 2;
       return 1;
-    };
-
-    drawGridLines(
-      canvas.current!,
-      clueWidth,
-      clueHeight,
-      totalWidth,
-      totalHeight,
-      (a, i) => {
-        const j = i - [clueWidth, clueHeight][a];
-        const d = j - cursor[a];
-        if (0 <= d && d <= 1) return 0;
-        return getLineWidth(a, i);
-      },
-      () => "rgb(var(--color-foreground))"
-    );
-
-    canvas.current!.drawRect(
-      clueWidth + cursor[0],
-      0,
-      1,
-      totalHeight,
-      `rgb(var(--color-primary) / 0.15)`
-    );
-    canvas.current!.drawRect(
-      0,
-      clueHeight + cursor[1],
-      totalWidth,
-      1,
-      `rgb(var(--color-primary) / 0.15)`
-    );
-
-    for (let a = 0; a < 2; a++) {
-      for (let di = 0; di < 2; di++) {
-        const p = cursor.map(
-          (c, ca) => c + [clueWidth, clueHeight][ca] + di
-        ) as Point;
-        const points: Point[] = [[...p], [...p]];
-        points[0][+!a] = -Infinity;
-        points[1][+!a] = Infinity;
-        canvas.current!.drawLine(
-          points,
-          getLineWidth(a, p[a]),
-          `rgb(var(--color-primary))`
-        );
-      }
-    }
-  }, [clueWidth, clueHeight, totalWidth, totalHeight, width, height, cursor]);
+    },
+    [width, height]
+  );
 
   useEffect(() => {
     canvas.current = new Canvas2D(
       canvasEl.current!,
-      [0, 0, totalWidth, totalHeight],
+      [0, 0, width + clueWidth, height + clueHeight],
       [0, 0, 1, 1]
     );
-    draw();
-  }, [totalWidth, totalHeight, draw]);
+    for (let a = 0; a < 2; a++) {
+      for (let i = 0; i <= [width, height][a]; i++) {
+        let points: Point[] = [
+          [-Infinity, -Infinity],
+          [Infinity, Infinity],
+        ];
+        points[0][a] = [clueWidth, clueHeight][a] + i;
+        points[1][a] = [clueWidth, clueHeight][a] + i;
+        if (i === cursor[a] || i === cursor[a] + 1) continue;
+        canvas.current.add(
+          new Line({
+            points,
+            width: getLineWidth(a, i),
+            stroke: "rgb(var(--color-foreground))",
+          })
+        );
+      }
+    }
+    for (let a = 0; a < 2; a++) {
+      let p = [0, 0];
+      p[a] = [clueWidth, clueHeight][a] + cursor[a];
+      const [x, y] = p;
+      let d = [1, 1];
+      d[+!a] = [clueWidth, clueHeight][+!a] + [width, height][+!a];
+      canvas.current.add(
+        new Rect({
+          x,
+          y,
+          width: d[0],
+          height: d[1],
+          fill: "rgb(var(--color-primary) / 0.15)",
+        })
+      );
+    }
+    for (let a = 0; a < 2; a++) {
+      for (let i = cursor[a]; i <= cursor[a] + 1; i++) {
+        let points: Point[] = [
+          [-Infinity, -Infinity],
+          [Infinity, Infinity],
+        ];
+        points[0][a] = [clueWidth, clueHeight][a] + i;
+        points[1][a] = [clueWidth, clueHeight][a] + i;
+        canvas.current.add(
+          new Line({
+            points,
+            width: getLineWidth(a, i),
+            stroke: "rgb(var(--color-primary))",
+          })
+        );
+      }
+    }
+  }, [width, height, clueWidth, clueHeight, cursor, getLineWidth]);
 
   useEffect(() => {
-    draw();
-  }, [canvasDim, draw]);
+    canvas.current!.draw();
+  }, [canvasDim]);
 
   return (
     <canvas
