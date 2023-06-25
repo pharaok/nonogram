8e5767873ce3419c761ac117ba401917d8676409
pharaok <farouk.tamer03@gmail.com>
2023-06-26 02:38:26 +0300

    fix solution image data url

--- src/components/solution.tsx
+++ src/components/solution.tsx
@@ -1,13 +1,17 @@
-import { Rect } from "helpers/canvas";
+import Canvas2D, { Rect } from "helpers/canvas";
 import { forwardRef, useState } from "react";
 import useNonogramStore, { selectDimensions } from "store";
 import Layer from "./canvas/layer";
 import Canvas from "./canvas";
+import { Write } from "types";
 
 const Solution = forwardRef<
-  HTMLCanvasElement,
-  React.ComponentPropsWithoutRef<"div">
->(function Solution({ style, ...props }, ref) {
+  Canvas2D | null,
+  Write<
+    React.ComponentPropsWithoutRef<"div">,
+    { onImageURLChange: (url: string) => void }
+  >
+>(function Solution({ onImageURLChange, style, ...props }, ref) {
   const solution = useNonogramStore((state) => state.solution);
   const colors = useNonogramStore((state) => state.colors);
   const [hue] = useState(Math.floor(Math.random() * 360));
@@ -39,6 +43,7 @@ const Solution = forwardRef<
                 canvas.add(new Rect({ x, y, width: 1, height: 1, fill: grad }));
             });
           });
+          onImageURLChange(canvas.ctx.canvas.toDataURL("image/png"));
         }}
       </Layer>
     </Canvas>
--- src/components/winModal.tsx
+++ src/components/winModal.tsx
@@ -1,31 +1,32 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Copy, Download } from "lucide-react";
+import NextLink from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import useNonogramStore, { selectDimensions, selectSeed } from "store";
+import Link from "./link";
 import Modal from "./modal";
 import Solution from "./solution";
-import NextLink from "next/link";
-import Link from "./link";
 
-const Content = () => {
+export default function WinModal({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
   const pathname = usePathname();
   const [width, height] = useNonogramStore(selectDimensions);
 
   const seed = useNonogramStore(selectSeed);
-  const [downloadLink, setDownloadLink] = useState("#");
-  const solutionEl = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    setDownloadLink(solutionEl.current!.toDataURL("image/png"));
-  }, []);
+  const [imageURL, setImageURL] = useState("");
 
   return (
-    <>
+    <Modal open={open} onOpenChange={onOpenChange} title="You won!">
       <div className="m-4 flex w-full flex-col items-center justify-center bg-background-alt p-4 shadow-xl shadow-black/25">
         <Solution
-          ref={solutionEl}
           className="mb-4 w-full border border-black"
+          onImageURLChange={setImageURL}
         />
         <button
           onClick={() => {
@@ -41,7 +42,7 @@ const Content = () => {
         <NextLink
           className="stroke rounded-full border-2 border-blue-700 p-2 text-blue-700 hover:bg-blue-700 hover:text-foreground"
           download={`${seed}.png`}
-          href={downloadLink}
+          href={imageURL}
           target="_blank"
         >
           <Download size={24} strokeWidth={2} />
@@ -61,20 +62,6 @@ const Content = () => {
           Play Again
         </Link>
       </Dialog.Close>
-    </>
-  );
-};
-
-export default function WinModal({
-  open,
-  onOpenChange,
-}: {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-}) {
-  return (
-    <Modal open={open} onOpenChange={onOpenChange} title="You won!">
-      <Content />
     </Modal>
   );
 }
